f918285fdf0817abfd29358929b83cc9495004b6
Krzysztof Szczerbowski <krzysiek@Krzysztofs-MacBook-Air.local>
2019-09-15 01:19:10 +0200

    New frontend project added

--- src_front/frontEndPage/app/src/main/java/com/example/frontendpage/MainActivity.java
+++ src_front/frontEndPage/app/src/main/java/com/example/frontendpage/MainActivity.java
@@ -1,44 +1,55 @@
+package com.example.frontendpage;
+
+import android.Manifest;
 import android.app.Activity;
+import android.app.AppComponentFactory;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.frontendpage.R;
 import com.example.frontendpage.networkutils.NetworkCheckout;
 import com.example.frontendpage.networkutils.NetworkUtils;
-import java.io.File
+import java.io.File;
 import java.io.IOException;
 
-public class MyCameraActivity extends Activity
-{
+public class MainActivity extends AppCompatActivity {
+
     private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
+    private ImageView imageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
-        Button photoButton = (Button) this.findViewById(R.id.button1);
-        photoButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-                {
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-                }
-                else
-                {
-                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                }
-            }
-        });
+        setContentView(R.layout.activity_main);
+
+//        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+//        Button photoButton = (Button) this.findViewById(R.id.button1);
+//        photoButton.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+//                {
+//                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+//                }
+//                else
+//                {
+//                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+//                }
+//            }
+//        });
     }
 
     @Override
@@ -59,13 +70,13 @@ public class MyCameraActivity extends Activity
             }
         }
 
-        protected void onActivityResult(int requestCode, int resultCode, Intent data)
-        {
-            if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
-            {
-                Bitmap photo = (Bitmap) data.getExtras().get("data");
-                imageView.setImageBitmap(photo);
-            }
-        }
+//        protected void onActivityResult(int requestCode, int resultCode, Intent data)
+//        {
+//            if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+//            {
+//                Bitmap photo = (Bitmap) data.getExtras().get("data");
+//                imageView.setImageBitmap(photo);
+//            }
+//        }
     }
 }
