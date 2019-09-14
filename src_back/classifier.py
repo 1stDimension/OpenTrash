542797bbac11cc67a1f577644de56e8e9971acfa
1stDimension <23144389+1stDimension@users.noreply.github.com>
2019-09-15 01:40:31 +0200

    w:x

--- src_back/classifier.py
+++ src_back/classifier.py
@@ -7,18 +7,24 @@ from torchvision import transforms
 
 
 def classify(image, labels):
-    alexnet = models.alexnet(pretrained=True)
+    model = models.googlenet(pretrained=True)
     transform = transforms.Compose([
-        transforms.Resize(512),
-        transforms.CenterCrop(256),
-        transforms.ToTensor()
-        ])
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )])
     img = Image.open(io.BytesIO(image))
     img_t = transform(img)
-    batch_t = torch.unsqueeze(img_t, 0)
+    batch = img_t.unsqueeze(0)
 
-    alexnet.eval()
-    out = alexnet(batch_t)
+    model.eval()
+
+    #with torch.no_grad():
+    #    output  = model(batch)
+    out = model(batch)
     _, index = torch.max(out, 1)
 
     return labels[index]
