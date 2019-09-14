922556507563142a8273342cb03c25b7c30ac2e2
Krzysztof Szczerbowski <krzysiek@Krzysztofs-MacBook-Air.local>
2019-09-14 22:35:08 +0200

    Add NetworkUtils class with networking utils, Utils with netwroking status features and NetworkCheckout to use it

--- src_front/frontEndPage/app/src/main/java/com/example/frontendpage/MainActivity.java
+++ src_front/frontEndPage/app/src/main/java/com/example/frontendpage/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.frontendpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.frontendpage.networkutils.NetworkCheckout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NetworkCheckout.getMacAddr();
     }
 }
