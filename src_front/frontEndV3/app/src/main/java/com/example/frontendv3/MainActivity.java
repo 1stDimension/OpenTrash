883a41257db447e266972b2b5b33ce3a90dfd2a2
Krzysztof Szczerbowski <krzysiek@Krzysztofs-MacBook-Air.local>
2019-09-15 09:45:08 +0200

    Add (broken) upload to server, add (broken) camera

--- src_front/frontEndV3/app/src/main/java/com/example/frontendv3/MainActivity.java
+++ src_front/frontEndV3/app/src/main/java/com/example/frontendv3/MainActivity.java
@@ -2,16 +2,37 @@ package com.example.frontendv3;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int GALLERY_REQUEST_CODE = 101;
+    private static final int CAMERA_REQUEST_CODE = 1888;
+
+    private String cameraFilePath;
 
     private ImageView mObrazek;
 
@@ -21,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
 
+        //captureFromCamera();
         pickFromGallery();
     }
 
@@ -43,8 +65,78 @@ public class MainActivity extends AppCompatActivity {
                 case GALLERY_REQUEST_CODE:
                     Uri selectedImage = data.getData();
                     mObrazek.setImageURI(selectedImage);
+
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+                    //Get the column index of MediaStore.Images.Media.DATA
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    //Gets the String value in the column
+                    String imgDecodableString = cursor.getString(columnIndex);
+                    Log.i("IMAGE", imgDecodableString);
+                    uploadToServer(imgDecodableString);
+                    break;
+
+                case CAMERA_REQUEST_CODE:
+                    mObrazek.setImageURI(Uri.parse(cameraFilePath));
                     break;
             }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        Log.d("FILENAME", imageFileName);
+
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Camera");
+        Log.d("FILE", storageDir.getPath());
+
+//        File image = File.createTempFile(imageFileName,".jpg", storageDir);
+        File image = File.createTempFile("name", ".jpg");
+        Log.d("FILE", image.getAbsolutePath());
+
+        cameraFilePath = "file://" + image.getAbsolutePath();
+        Log.d("FILEPATH", cameraFilePath);
+        return image;
+    }
+
+    private void captureFromCamera() {
+        Log.d("INTENT", "Camera: caputer starting..." );
+
+        try {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", createImageFile()));
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            Log.d("INTENT", "Camera: " + intent.getData().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("INTENT", "Couldn't make an Intent");
+        }
+    }
+
+    private void uploadToServer(String filePath) {
+        Retrofit retrofit = NetworkClient.getRetrofitClient(this);
+        UploadAPIs uploadAPIs = retrofit.create(UploadAPIs.class);
+        //Create a file object using file path
+        File file = new File(filePath);
+        // Create a request body with file and image media type
+        RequestBody fileReqBody = RequestBody.create(MediaType.parse("image/*"), file);
+        // Create MultipartBody.Part using file request-body,file name and part name
+        MultipartBody.Part part = MultipartBody.Part.createFormData("upload", file.getName(), fileReqBody);
+        //Create request body with text description and text media type
+        RequestBody description = RequestBody.create(MediaType.parse("text/plain"), "image-type");
+        //
+        Call call = uploadAPIs.uploadImage(part, description);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+            }
+            @Override
+            public void onFailure(Call call, Throwable t) {
+            }
+        });
     }
 }
---
+++ src_front/frontEndV3/app/src/main/java/com/example/frontendv3/NetworkClient.java
@@ -0,0 +1,24 @@
+package com.example.frontendv3;
+
+import android.content.Context;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkClient {
+    private static final String BASE_URL = "10.250.195.32:5000";
+    private static Retrofit retrofit;
+    public static Retrofit getRetrofitClient(Context context) {
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
