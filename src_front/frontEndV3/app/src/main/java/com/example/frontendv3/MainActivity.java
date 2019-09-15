c5bfcb381918dcc1b386e04eacf8d2e010f7a1f6
Krzysztof Szczerbowski <krzysiek@Krzysztofs-MacBook-Air.local>
2019-09-15 06:12:37 +0200

    Add use permission for Internet and Storage. Add method for selecting image from gallery

--- src_front/frontEndV3/app/src/main/java/com/example/frontendv3/MainActivity.java
+++ src_front/frontEndV3/app/src/main/java/com/example/frontendv3/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.frontendv3;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int GALLERY_REQUEST_CODE = 101;
+
+    private ImageView mObrazek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        pickFromGallery();
+    }
+
+    private void init() {
+        mObrazek = (ImageView) findViewById(R.id.iv_obrazek);
+    }
+
+    private void pickFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == Activity.RESULT_OK) {
+            switch(requestCode) {
+                case GALLERY_REQUEST_CODE:
+                    Uri selectedImage = data.getData();
+                    mObrazek.setImageURI(selectedImage);
+                    break;
+            }
+        }
     }
 }
