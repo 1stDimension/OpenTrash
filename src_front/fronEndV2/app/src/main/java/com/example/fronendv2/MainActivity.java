8cb3a5d12a6008fdd06792d6d6f4b0dc729a590d
Krzysztof Szczerbowski <krzysiek@Krzysztofs-MacBook-Air.local>
2019-09-15 04:54:53 +0200

    Add uses-permission for INTERNET and using external storage, add Intent for img gallery picking

--- src_front/fronEndV2/app/src/main/java/com/example/fronendv2/MainActivity.java
+++ src_front/fronEndV2/app/src/main/java/com/example/fronendv2/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.fronendv2;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int GALLERY_REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pickFromGallery();
+    }
+
+    private void pickFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
     }
 }
