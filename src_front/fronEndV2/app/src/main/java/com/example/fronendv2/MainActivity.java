845f564f8d016f6c71455ac15dfce6eaf61c36cf
Krzysztof Szczerbowski <krzysiek@Krzysztofs-MacBook-Air.local>
2019-09-15 05:44:07 +0200

    Make another verioson of frontend beacuse why not?

--- src_front/fronEndV2/app/src/main/java/com/example/fronendv2/MainActivity.java
+++ src_front/fronEndV2/app/src/main/java/com/example/fronendv2/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.fronendv2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,12 +16,20 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int GALLERY_REQUEST_CODE = 101;
 
+    private ImageView mObrazek;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+
         pickFromGallery();
+    }
+
+    private void init() {
+//        mObrazek = (ImageView) findViewById(R.id.iv_image);
     }
 
     private void pickFromGallery() {
@@ -26,5 +38,17 @@ public class MainActivity extends AppCompatActivity {
         String[] mimeTypes = {"image/jpeg", "image/png"};
         intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
         startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == Activity.RESULT_OK) {
+            switch(requestCode) {
+                case GALLERY_REQUEST_CODE:
+                    Uri selectedImage = data.getData();
+                    //mObrazek.setImageURI(selectedImage);
+                    break;
+            }
+        }
     }
 }
